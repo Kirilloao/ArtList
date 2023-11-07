01774bdbcaee3814775f37363856376bc603c1db
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-07 16:38:21 +0100

    use NetworkManager in AuthorsListViewController

--- ArtList/Controllers/AuthorsListViewController.swift
+++ ArtList/Controllers/AuthorsListViewController.swift
@@ -14,16 +14,18 @@ final class AuthorsListViewController: UITableViewController {
         UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"),
         UIImage(named: "6")
     ]
-
+    
+    var artists: [Author] = []
+    
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         setTableView()
         setNavigationBar()
+        fetchAuthors(with: Links.api.rawValue)
     }
     
     // MARK: - Private Methods
-    
     private func setNavigationBar() {
         title = "Authors"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -36,21 +38,38 @@ final class AuthorsListViewController: UITableViewController {
     
     // MARK: - UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        authors.count
+        artists.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: AuthorCell.reuseID, for: indexPath) as? AuthorCell else { return UITableViewCell() }
         
-        let authorImage = authors[indexPath.row]
-        cell.configure(with: authorImage!)
-    
+        let author = artists[indexPath.row]
+        cell.configure(with: author)
+        
         return cell
     }
     
     // MARK: - UITableViewDelegate
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        150
+        UITableView.automaticDimension
+    }
+}
+
+// MARK: - Networking
+extension AuthorsListViewController {
+    private func fetchAuthors(with url: String) {
+        NetworkManager.shared.fetch(with: url) { result in
+            switch result {
+            case .success(let data):
+                self.artists = data.artists
+                self.tableView.reloadData()
+                
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
